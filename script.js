efe1d9df2180c6f3b6bd36f9834120e21cbb52a0
Krzysztof <ki.zwolak@gmail.com>
2022-08-24 01:37:15 +0200

    Calculator skeleton, fixes to come

---
+++ script.js
@@ -0,0 +1,91 @@
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+function operate(a, operator, b) {
+    if (operator === "-") {
+        return subtract(a, b);
+    } else if (operator === "+") {
+        return add(a, b);
+    } else if (operator === "*") {
+        return multiply(a, b);
+    } else if (operator === "/") {
+        return divide(a, b);
+    } else {
+        return "Wrong operator!";
+    }
+}
+
+
+let numberButtons = document.querySelectorAll(".numberbutton");
+let screen = document.querySelector('#screen');
+let number = document.createTextNode('');
+screen.appendChild(number);
+let clear = document.querySelector('.clear');
+let equal = document.querySelector(".equal");
+let result = 0;
+let operatorButtons = document.querySelectorAll(".operator");
+let operator;
+let numbers = [];
+
+
+for (let i = 0; i < numberButtons.length; i++) {
+    numberButtons[i].addEventListener('click', function(e) {
+        number.textContent += e.currentTarget.textContent;
+    });
+}
+
+
+
+for (let i = 0; i < operatorButtons.length; i++) {
+    operatorButtons[i].addEventListener('click', function(e) {
+        operator = e.currentTarget.textContent;
+        if (number == result) {
+            return operator;
+        }
+        numbers.push(screen.textContent);
+        number.textContent = '';
+        screen.appendChild(number);
+    });
+}
+
+
+clear.addEventListener('click', function() {
+    operator = '';
+    numbers = [];
+    number.textContent = '';
+    screen.appendChild(number);
+});
+
+
+function calculate() {
+    //funkcja zatrzymuje wynik z powodu kodu na dole numbers.push - wyczysc ekran
+    numbers.push(screen.textContent);
+    let a = parseInt(numbers[0]);
+    let b = parseInt(numbers[1]);
+    console.log(a);
+    console.log(b);
+    result = (operate(a, operator, b));
+    screen.textContent = result;
+    //dokoncz kalkulacje po otrzymaniu wyniku (kalkulacja na podstawie wyniku)
+    numbers = [];
+    operator = '';
+    numbers.push(result);
+    console.log(numbers);
+}
+
+
+equal.addEventListener('click', calculate);
+
